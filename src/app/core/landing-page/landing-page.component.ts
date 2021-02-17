4a4a5da5e36b28d2eba0ea51c82f2b5c6d68db1f
Andrew McCarthy <andrewmcc1120@gmail.com>
2021-02-17 14:02:19 -0500

    text animation on landing page

--- src/app/core/landing-page/landing-page.component.ts
+++ src/app/core/landing-page/landing-page.component.ts
@@ -10,6 +10,26 @@ export class LandingPageComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
+    const checkpoint = 300;
+    const maxMargin = 300;
+    let opacity = 1;
+    let marginTop = 0;
+    let prevVerticalPos = 0;
+    window.addEventListener('scroll', () => {
+      const verticalPos = window.pageYOffset;
+      if (checkpoint >= verticalPos) {
+        opacity = 1 - (verticalPos / checkpoint);
+      } else {
+        opacity = 0;
+      }
+      if (verticalPos >= prevVerticalPos && marginTop > -1) {
+        marginTop = marginTop < maxMargin ? verticalPos : maxMargin;
+      } else {
+        marginTop = verticalPos > 0 ? verticalPos : 0;
+      }
+      prevVerticalPos = verticalPos;
+      document.getElementById('scroll-text-effect').style.opacity = opacity.toString();
+      document.getElementById('scroll-text-effect').style.marginTop = `${marginTop.toString()}px`;
+    });
   }
-
 }
